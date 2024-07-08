b68eb31e86817fdc501a2c0485f274444ed51317
Imtyaz Ahmed <54882826+imtyaz-17@users.noreply.github.com>
2024-07-08 10:49:12 +0600

    T6 Update ShopController.php

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -15,7 +15,12 @@ class ShopController extends Controller
         // TASK: resize the uploaded image from /storage/app/shops/$filename
         //   to size of 500x500 and store it as /storage/app/shops/resized-$filename
         // Use intervention/image package, it's already pre-installed for you
-
+        $imagePath = storage_path('app/shops/' . $filename);
+        $resizedImagePath = storage_path('app/shops/resized-' . $filename);
+        Image::make($imagePath)
+            ->resize(500, 500)
+            ->save($resizedImagePath);
+        
         return 'Success';
     }
 }
